6bb88a9f6a31cc66c877470b9e4b26e5c3510120
mths0303s <matheusaraujo1460@gmail.com>
2024-11-01 22:13:09 -0300

    Adding the While Loop

--- Script2.py
+++ Script2.py
@@ -1,39 +1,41 @@
 import chromadb
 from openai import OpenAI
 
-api_key = ""
+api_key = "Coloque sua chave API"
 client = OpenAI(api_key=api_key)
 
-questao = input("""Como posso lhe ajudar?
+while True:
+    questao = input("Como posso lhe ajudar?\nDigite 'sair' para encerrar.\n\n")
 
-""")
+    if questao.lower() == "sair":
+        print("\n\nObrigado por usar o assistente do Restaurante Sabores! Até a próxima!")
+        break
 
-chroma_client = chromadb.Client()
-chroma_client = chromadb.PersistentClient(path="db")
-colletion = chroma_client.get_or_create_collection(name="artigo")
+    chroma_client = chromadb.Client()
+    chroma_client = chromadb.PersistentClient(path="db")
+    colletion = chroma_client.get_or_create_collection(name="artigo")
 
-results = colletion.query(query_texts=questao, n_results=2)
+    results = colletion.query(query_texts=questao, n_results=2)
+    if results and "documents" in results and results["documents"]:
+        conteudo = results["documents"][0][0] + results ["documents"][0][1]
+    else:
+        conteudo = "Desculpe, mas não consigo ajudar. Você Tem alguma outra pergunta?"
 
-conteudo = results["documents"][0][0] + results ["documents"][0][1]
+    prompt = """
+    Você é um assistente educado e útil do Restaurante Sabores.
+    Use o seguinte contexto para responder a questão, não use nenhuma informação adicional, se não houver informação no contexto, responda: Desculpe mas não consigo ajudar com isso.
+    Depois de responder a pergunta, diga: Fico feliz por você ter usado nossos serviços, se houver mais dúvidas, sinta-se à vontade para perguntar.
+    """
 
-prompt = """
-Você é um assistente do Restaurante Sabores.
-Use o seguinte contexto para responder a questão, não use nenhuma informação adicional, se não houver informação no contexto, responda: Desculpe mas não consigo ajudar. Tem alguma outra pergunta?
-Sempre termine a resposta com: Foi um prazer falar com você, não deixe de provar nossos sabores e se houver outra pergunta, ficarei feliz em ajudá-lo.
-"""
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "system", "content": conteudo},
+            {"role": "user", "content": questao},
+        ],
+    )
 
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": prompt,
-        },
-        {"role": "system", "content": conteudo},
-        {"role": "user", "content": questao},
-    ],
-)
+    answer = completion.choices[0].message.content
 
-answer = completion.choices[0].message.content
-
-print("\n\n",answer,"\n\n")
+    print("\n\n",answer,"\n\n")
